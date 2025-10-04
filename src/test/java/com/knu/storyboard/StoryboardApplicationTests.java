949b41130aef7338cdeeeb78c7a5e522de932532
gitjiho <jiho99322@gmail.com>
2025-10-04 17:03:26 +0900

    Test: 의존성 추가 과정에서 생성된 오류 해결

--- src/test/java/com/knu/storyboard/StoryboardApplicationTests.java
+++ src/test/java/com/knu/storyboard/StoryboardApplicationTests.java
@@ -1,6 +1,10 @@
 package com.knu.storyboard;
 
+import com.knu.storyboard.config.MySQLTestContainerConfig;
+import com.knu.storyboard.config.RedisTestContainerConfig;
+import com.knu.storyboard.config.TestEnvironmentConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
