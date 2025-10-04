66833b7bbaf020b7e60fa61a186c1760f266d38d
gitjiho <jiho99322@gmail.com>
2025-10-04 17:01:02 +0900

    Test: 테스트 의존성 추가

--- src/test/java/com/knu/storyboard/StoryboardApplicationTests.java
+++ src/test/java/com/knu/storyboard/StoryboardApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith({RedisTestContainerConfig.class, MySQLTestContainerConfig.class, TestEnvironmentConfig.class})
 class StoryboardApplicationTests {
 
     @Test
