be35ba4e1e1e28424cb16fe75a337865b1f3afd3
toothlessdev <toothless042@gmail.com>
2025-11-18 01:17:03 +0900

    Refactor: OAuthController에서 `redirectUrl` 쿼리파라미터를 받도록 변경

--- src/main/java/com/knu/storyboard/auth/presentation/controller/OAuthController.java
+++ src/main/java/com/knu/storyboard/auth/presentation/controller/OAuthController.java
@@ -1,8 +1,8 @@
 package com.knu.storyboard.auth.presentation.controller;
 
-import com.knu.storyboard.auth.business.service.OAuthLoginService;
-import com.knu.storyboard.auth.presentation.api.OAuthApi;
-import lombok.RequiredArgsConstructor;
+import java.net.URI;
+import java.util.Optional;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.net.URI;
+import com.knu.storyboard.auth.business.service.OAuthLoginService;
+import com.knu.storyboard.auth.domain.DeviceType;
+import com.knu.storyboard.auth.domain.OAuthState;
+import com.knu.storyboard.auth.presentation.api.OAuthApi;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,11 +27,14 @@ public class OAuthController implements OAuthApi {
     @Override
     public RedirectView redirectToOAuthLoginPage(
             @PathVariable("provider") String provider,
-            @RequestParam(value = "state", required = false) String state) {
+            @RequestParam(value = "deviceType", required = false) String deviceType,
+            @RequestParam(value = "redirectUrl", required = false) String redirectUrl) {
 
-        String redirectUrl = oAuthLoginService.getOAuthLoginUrl(provider, state);
+        String state = OAuthState.of(DeviceType.fromString(Optional.ofNullable(deviceType).orElse(DeviceType.COMPUTER.name())), redirectUrl)
+                .toStateParameter();
 
-        return new RedirectView(redirectUrl);
+        String redirectViewUrl = oAuthLoginService.getOAuthLoginUrl(provider, state);
+        return new RedirectView(redirectViewUrl);
     }
 
     @Override
